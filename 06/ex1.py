e2a3040986690403217c4521c2bc2cf4a938c4eb
ArktinenKarpalo <arktinenkarpalo@gmail.com>
2021-12-06 09:24:17 +0200

    Day 6

---
+++ 06/ex1.py
@@ -0,0 +1,17 @@
+import sys
+import collections
+input = list(map(int, sys.stdin.read().split(",")))
+
+fish = collections.defaultdict(int)
+for x in input:
+    fish[x] += 1
+for _ in range(80):
+    fish2 = collections.defaultdict(int)
+    for x, cnt in fish.items():
+        if x == 0:
+            fish2[6] += cnt
+            fish2[8] += cnt
+        else:
+            fish2[x-1] += cnt
+    fish = fish2
+print(sum(fish.values()))
