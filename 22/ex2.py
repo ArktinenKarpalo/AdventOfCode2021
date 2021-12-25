e4a4f36163196fe9e731ab4f317293d70db6a55e
ArktinenKarpalo <arktinenkarpalo@gmail.com>
2021-12-25 08:18:16 +0200

    Fix ex2

--- 22/ex2.py
+++ 22/ex2.py
@@ -1,5 +1,5 @@
 import sys
-import bisect
+import itertools
 import re
 input = sys.stdin.read().strip().split("\n")
 sm = 0
@@ -8,41 +8,28 @@ for x in input:
     rs = re.match("(.*) x=(.*)\.\.(.*),y=(.*)\.\.(.*),z=(.*)\.\.(.*)", x).groups()
     rs = [rs[0]]+[int(x) for x in rs[1:]]
     cbd.append(rs)
-cbd2 = list(cbd)
-for ao in range(3):
-    cbd = list(map(lambda x: [x[0]]+x[5:7]+x[1:5], cbd))
-    cbd2 = list(map(lambda x: [x[0]]+x[5:7]+x[1:5], cbd2))
-    cts = []
-    for cc in cbd:
-        for ct in [cc[1]-1, cc[2]]:
-            cts.append(ct)
-    cts = sorted(set(cts))
-    cn = []
-    print(len(cbd2))
-    for c2 in cbd2:
-        cur = c2
-        cbd3 = []
-        for ct in cts[bisect.bisect_left(cts, c2[1]):bisect.bisect_right(cts, c2[1+1])+1]:
-            if cur[1] <= ct and ct < cur[2]:
-                c2 = list(cur)
-                c2[2] = ct
-                cur[1] = ct+1
-                cbd3.append(c2)
-        cbd3.append(cur)
-        cn += cbd3
-    cbd2 = cn
-print(len(cbd2))
 
-on = set()
-for c in cbd2:
-    if c[0] == "on":
-        on.add(tuple(c[1:]))
-    else:
-        if tuple(c[1:]) in on:
-            on.remove(tuple(c[1:]))
+def cn(dim, cbdf, sz):
+    if len(cbdf) == 0:
+        return
+    global sm
+    if dim == 3:
+        if cbdf[-1][0] == "on":
+            sm += sz
+        return
+    lst = sorted(list(itertools.chain.from_iterable([x[1+dim*2], x[1+dim*2+1]] for x in cbdf)))
+    lt = lst[0]-1
+    for p in lst:
+        if p+1 == lt:
+            continue
+        if p != lt:
+            cbdf2 = list(filter(lambda x: x[1+dim*2] <= lt and lt <= x[1+dim*2+1], cbdf))
+            cn(dim+1, cbdf2, sz*(p-lt))
+        cbdf2 = list(filter(lambda x: x[1+dim*2] <= p and p <= x[1+dim*2+1], cbdf))
+        cn(dim+1, cbdf2, sz*(1))
+        lt = p+1
 
-for c in on:
-    sm += (c[1]-c[0]+1)*(c[3]-c[2]+1)*(c[5]-c[4]+1)
 
+cn(0, cbd, 1)
 
 print(sm)
